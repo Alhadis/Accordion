a729cd77b386fde29fd95753b1728242776a4d8c
Alhadis <gardnerjohng@gmail.com>
2015-12-29 00:57:59 +1100

    Decouple demo configurations from shared script
    This allows more flexible testing of different option-sets.

--- demos/assets/shared.js
+++ demos/assets/shared.js
@@ -1,19 +1,18 @@
 Function.prototype.debounce=function(limit,soon){var fn=this,limit=limit<0?0:limit,started,context,args,timer,delayed=function(){var timeSince=Date.now()-started;if(timeSince>=limit){if(!soon)fn.apply(context,args);if(timer)clearTimeout(timer);timer=context=args=null}else timer=setTimeout(delayed,limit-timeSince)};return function(){context=this,args=arguments;if(!limit)return fn.apply(context,args);started=Date.now();if(!timer){if(soon)fn.apply(context,args);timer=setTimeout(delayed,limit)}}};
 
 
-
-var each        = Array.prototype.forEach;
 var accordions  = [];
+function runDemo(config){
+	var each          = Array.prototype.forEach;
+	config            = config || {};
+	config.autoResize = false;
 
-each.call(document.querySelectorAll(".accordion"), function(el){
-	accordions.push(new Accordion(el, {
-		autoResize: false,
-		animHeight: true
-	}));
-});
+	each.call(document.querySelectorAll(".accordion"), function(el){
+		accordions.push(new Accordion(el, config));
+	});
 
-window.addEventListener("resize", function(e){
-	for(var i = 0, l = accordions.length; i < l; ++i){
-		accordions[i].update();
-	}
-}.debounce(100));
+	window.addEventListener("resize", function(e){
+		for(var i = 0, l = accordions.length; i < l; ++i)
+			accordions[i].update();
+	}.debounce(100));
+}
